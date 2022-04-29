af129c88d6a5cf684a391231107dc2dcfc8ae89c
reinaw1012 <reinaw1012@berkeley.edu>
2022-04-29 00:09:28 +0000

    lint

--- alpa/testing.py
+++ alpa/testing.py
@@ -216,12 +216,12 @@ class PipelineBasicTest(unittest.TestCase):
                 test_remat=False,
                 pipeline_stage_mode="uniform_layer_gpipe",
                 do_numerical_test=True,
-                return_value=False, 
+                return_value=False,
                 num_micro_batches=None):
         virtual_mesh = DeviceCluster().get_virtual_physical_mesh()
         set_parallelize_options(devices=virtual_mesh,
                                 strategy="pipeshard_parallel",
-                                pipeline_stage_mode=pipeline_stage_mode, 
+                                pipeline_stage_mode=pipeline_stage_mode,
                                 num_micro_batches=num_micro_batches)
 
         # Init model and optimizer
